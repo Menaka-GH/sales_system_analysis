35be75bda5871390bbfe0a65b6d8ea8d718f671d
Menaka-GH <menakasuthankk@gmail.com>
2021-03-08 18:29:00 -0500

    functions for update, delete and  sales analysis are added

--- salesdb_backend.py
+++ salesdb_backend.py
@@ -27,3 +27,47 @@ def viewsalesData():
     rows = cur.fetchall()
     con.close()
     return rows
+def deletesalesData(Order_ID):
+    con = sqlite3.connect("sale.db")
+    cur = con.cursor()
+    cur.execute("DELETE FROM salesTable WHERE Order_ID=?", (Order_ID))
+    con.commit()
+    con.close()
+
+def searchData(Order_ID="",Product="", Quantity="",Price="", Order_date="", Address=""):
+    con = sqlite3.connect("sale.db")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM salesTable WHERE Order_ID=? OR Product=? OR Quantity=? OR Price=? OR Order_date=? OR Address=? ", (Order_ID, Product, Quantity, Price, Order_date, Address))
+    rows = cur.fetchall()
+    con.close()
+    return rows
+
+def updatedata():
+    con = sqlite3.connect("sale.db")
+    cur = con.cursor()
+    cur.execute(
+        "UPDATE salesTable SET Order_ID=?, Product=?, Quantity=?, Price=?, Order_date=?, Address=? ",\
+        (Order_ID, Product, Quantity, Price, Order_date, Address, Id))
+    con.commit()
+    con.close()
+
+def salesAnalysis():
+    con = sqlite3.connect("sale.db")
+    cur = con.cursor()
+    query = pd.read_sql_query("""
+    SELECT
+        Order_ID, Price
+    FROM
+        salesTable
+    ;""", con)
+    # for row in df:
+    #   print(row)
+    # df.head()
+    df = pd.DataFrame(query)
+    df.plot(kind="bar", x="Order_ID", y="Price")
+    plt.show()
+
+    con.commit()
+    con.close()
+
+#salesData()
